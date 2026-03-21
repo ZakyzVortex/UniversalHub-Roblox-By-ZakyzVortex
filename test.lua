18728a7df34fd9af081de15a0418d773941cddd0
ZakyzVortex <isaackrugboddenberg@gmail.com>
2026-03-20 21:41:34 -0300

    Atualizar o test.lua

--- test.lua
+++ test.lua
@@ -1,6 +1,3 @@
--- ================== UNIVERSAL HUB - WINDUI VERSION ==================
--- Universal Hub WindUI By ZakyzVortex (Mobile Optimized & Organized)
-
 -- ================== SERVICES ==================
 local Players         = game:GetService("Players")
 local RunService      = game:GetService("RunService")
